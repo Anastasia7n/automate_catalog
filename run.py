70146c0144e940fab4dd9cbf28b0c54c919455b8
My name <anastasia7n@github.com>
2022-06-25 17:24:34 -0700

    changed

--- run.py
+++ run.py
@@ -1,34 +1,24 @@
 #!/usr/bin/env python3
 
-import os
-import requests
+import os, sys
 import json
+import requests
+import requests as requests
 
+path = "supplier-data/descriptions/"
+url = "http://localhost/fruits/"
 
-def catalog_data(url, description_dir):
-    fruit = {}
-    for item in os.listdir(description_dir):
-        fruit.clear()
-        filename = os.path.join(description_dir, item)
-        with open(filename) as f:
-            line = f.readlines()
-            description = ""
-            for i in range(2, len(line)):
-                description = description + line[i].strip('\n').replace(u'\xa0', u'')
-            fruit["description"] = description
-            fruit["weight"] = int(line[1].strip('\n').strip('lbs'))
-            fruit["name"] = line[0].strip('\n')
-            fruit["image_name"] = (item.strip('.txt')) + '.jpeg'
-            print(fruit)
-            if url != "":
-                response = requests.post(url, json=fruit)
-                print(response.request.url)
-                print(response.status_code)
-    return 0
+files = os.listdir(path)
 
-
-if __name__ == '__main__':
-    url = 'http://localhost/fruits'
-    user = os.getenv('USER')
-    description_directory = '/home/{}/supplier-data/descriptions/'.format(user)
-    catalog_data(url, description_directory)
+for file in files:
+  if file.endswith("txt"):
+    with open(path + file, 'r') as f:
+      #grab the file name, ex. 001, 002 to use for image file
+      fruit_name = os.path.splitext(file)[0]
+      data = f.read()
+      data = data.split("\n")
+      fruit_dic = {"name": data[0], "weight": int(data[1].strip(" lbs")), "description": d$
+      response = requests.post(url, json=fruit_dic)
+      response.raise_for_status()
+      print(response.request.url)
+      print(response.status_code)
